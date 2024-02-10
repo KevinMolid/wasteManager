20b5a6e638d5f850d0549823dc253192d1ffebfe
Kevin Molid <kevinmolid@gmail.com>
2024-02-10 12:12:52 +0100

    Added truck and animation

---
+++ src/components/Truck.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Truck() {
+    return (
+        <div className='truck'>
+
+        </div>
+    )
+}
--- src/components/TruckArea.jsx
+++ src/components/TruckArea.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Truck from './Truck'
 
 export default function TruckArea() {
     return (
         <section className="truck-area">
-            
+            <Truck />
         </section>
     )
 }
